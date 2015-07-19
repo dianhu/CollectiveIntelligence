b84cee983f44eec94f5b40bdfad607460de13545
HCY <HCY@HCY-PC>
2015-07-19 21:49:21 +0800

    add

---
+++ chapter2/shell.py
@@ -0,0 +1,8 @@
+'''
+Created on 2015年7月19日
+
+@author: HCY
+'''
+from recommendations import critics
+#print(critics['Lisa Rose']['Lady in the Water'])
+print(critics['Toby'])
--- chapter2/test.py
+++
@@ -1,6 +0,0 @@
-'''
-Created on 2015年7月19日
-
-@author: HCY
-'''
-print("Hello World");
